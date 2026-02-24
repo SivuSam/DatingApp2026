2676edfae4e0c192d24c37226b07236357e8546e
Sivuyile <Sivuyile.sam25@gmail.com>
2026-02-24 10:59:01 +0200

    Section 3-Video 27

--- API/Program.cs
+++ API/Program.cs
@@ -13,10 +13,12 @@ builder.Services.AddDbContext<AppDbContext>(Opt =>
 {
     Opt.UseSqlite(builder.Configuration.GetConnectionString("DefalutConnection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
