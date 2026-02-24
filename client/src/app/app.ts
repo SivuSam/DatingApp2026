43872e389e41e0cb6eb4684fba24fff9fd7f5fac
Sivuyile <Sivuyile.sam25@gmail.com>
2026-02-24 13:27:08 +0200

    End of Section 3

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -12,19 +12,15 @@ import { lastValueFrom } from 'rxjs';
 export class App implements OnInit{
   private http=inject(HttpClient);
   protected title='Dating App';
-  protected members=signal<any>({})
+  protected members=signal<any>([])
   
   async ngOnInit(){
- this.http.get('https://localhost:5001/api/members').subscribe({
-  next: response=> this.members.set(response),
-  error: error=> console.log(error),
-  complete: ()=> console.log('Completed the http request')
- })
-
-}
-  async getMembers(){
+ this.members.set(await this.getMembers())
+  
+ }
+ async getMembers(){
     try {
-      return await lastValueFrom(this.http.get('https://localhost:5001/api/members'))
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'))
     } catch (error) {
       console.log(error)
       throw error
@@ -33,4 +29,7 @@ export class App implements OnInit{
  }
 
 }
+  
+
+
 
